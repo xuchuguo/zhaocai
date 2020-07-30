5b6e73a210756cac7216aacf6310bae4345819f2
xuchuguo <xuchuguo@qq.com>
2020-07-30 21:58:36 +0800

    新增404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import VueRouter, { RouteConfig } from 'vue-router'
 import Money from '@/views/Money.vue'
 import Labels from '@/views/Labels.vue'
 import Statistics from '@/views/Statistics.vue'
+import Notfound from '@/views/Notfound.vue'
 
 Vue.use(VueRouter)
 
@@ -25,6 +26,10 @@ const routes: Array<RouteConfig> = [
     path: '/statistics',
     name: 'Statistics',
     component: Statistics
+  },
+  {
+    path: '*',
+    component: Notfound
   }
 ]
 
---
+++ src/views/Notfound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    你当前页面不存在，请检查网址是否正确
+    <div>
+      <router-link to="/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
