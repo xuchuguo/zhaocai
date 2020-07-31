977b5cec265b01c803b409fbbfd42d2b7b5d54f3
xuchuguo <xuchuguo@qq.com>
2020-07-31 18:54:05 +0800

    注册Icon组件

---
+++ src/components/Icon.vue
@@ -0,0 +1,15 @@
+<template>
+    <svg>
+        <use xlink:href="#money"></use>
+    </svg>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
