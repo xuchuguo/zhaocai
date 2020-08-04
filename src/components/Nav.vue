7d6e54c0db7e0ff22fd9ba8ee6005d7775d3376a
xuchuguo <xuchuguo@qq.com>
2020-08-04 11:14:09 +0800

    添加Nav样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="nav">
-    <router-link to="/money">
+    <router-link class="item" to="/money">
       <Icon name = 'money'></Icon>
       记账
     </router-link>
-    <router-link to="/labels">
+    <router-link class="item" to="/labels">
       <Icon name = 'label'></Icon>
       标签
     </router-link>
-    <router-link to="/statistics">
+    <router-link class="item" to="/statistics">
        <Icon name = 'tongji'></Icon>
       统计
     </router-link>
@@ -20,4 +20,21 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+.nav {
+  font-size: 12px;
+  display: flex;
+  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  flex-direction: row;
+  > .item{
+    width: 33.3333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    .icon {
+      width: 32px;
+      height: 32px;
+    }
+  }
+}
 </style>
